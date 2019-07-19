870244e51b9e02faae7c7e572f913a87bbc3b804
배민수 <kluge0221@gmail.com>
2019-07-19 17:58:56 +0900

    network define

--- chat/socket.js
+++ chat/socket.js
@@ -1,28 +1,35 @@
 
 const SocketIO = require('socket.io');
+const NetworkDefine = require('../constant/NetworkDefine')
 
 module.exports = (server) => {
 
-    const io = SocketIO(server, { path: '/' })
-
+    const io = SocketIO(server)
     const chat = io.of('/chat')
 
 
-    chat.on('connection', (socket) => {
+    io.on('connection', (socket) => {
         const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         console.log('new client connect', ip, socket.id, req.ip)
 
 
-        socket.on('join', (data) => {
-            socket.join(data.roomId)
+        socket.on(NetworkDefine.CHAT_ROOM_JOIN, (data) => {
+            console.log(data);
+            socket.join(data.roomId);
         })
 
-        socket.on('leave',(data) => {
-            socket.leave(data.roomId)
+        socket.on(NetworkDefine.CHAT_ROOM_LEAVE, (data) => {
+            console.log(data);
+            socket.leave(data.roomId);
         })
 
-    
+        socket.on(NetworkDefine.SEND_TEXT_MESSAGE, (data => {
+            console.log(data);
+            socket.to(data.roomId).emit(NetworkDefine.RECEIVE_TEXT_MESSAGE, data);
+        }))
+
+
     });
 
 }
---
+++ constant/NetworkDefine.js
@@ -0,0 +1,6 @@
+
+
+export const CHAT_ROOM_JOIN = "chat_room_connect";
+export const CHAT_ROOM_LEAVE = "chat_room_disconnect";
+export const SEND_TEXT_MESSAGE = "client_text_message_handle";
+export const RECEIVE_TEXT_MESSAGE = "text_message_from_server";
