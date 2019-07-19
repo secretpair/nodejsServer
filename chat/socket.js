90b070a252dd7063e266ff34ae69151b0f068717
배민수 <kluge0221@gmail.com>
2019-07-19 17:16:32 +0900

    git ignore

--- chat/socket.js
+++ chat/socket.js
@@ -1,6 +1,28 @@
 
-const Io = require('socket.io');
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
+
+    const io = SocketIO(server, { path: '/' })
+
+    const chat = io.of('/chat')
+
+
+    chat.on('connection', (socket) => {
+        const req = socket.request;
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log('new client connect', ip, socket.id, req.ip)
+
+
+        socket.on('join', (data) => {
+            socket.join(data.roomId)
+        })
+
+        socket.on('leave',(data) => {
+            socket.leave(data.roomId)
+        })
+
+    
+    });
 
 }
